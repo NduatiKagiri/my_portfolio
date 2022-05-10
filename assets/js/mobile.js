ad2380fcc1ac465392898dc603003b8861398b5e
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-05-10 22:10:48 +0300

    Design the mobile menu

---
+++ assets/js/mobile.js
@@ -0,0 +1,9 @@
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links-list');
+	const navLinks = document.querySelectorAll('.nav-links-list li');
+
+	
+}
+
+navSlide();
