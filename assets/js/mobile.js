ef04634cc64aadc4d3dc75544979ac9f475b8366
darikmohammed <r.m.darik@gmail.com>
2022-05-10 22:18:05 +0300

    Add JavaScript for the mobile menu with toggling the animation

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -3,7 +3,36 @@ const navSlide = () => {
 	const nav = document.querySelector('.nav-links-list');
 	const navLinks = document.querySelectorAll('.nav-links-list li');
 
-	
+	burger.addEventListener('click', () => {
+
+		nav.classList.toggle('nav-active');
+
+		navLinks.forEach((link, index) => {
+			if (link.style.animation){
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+			}
+		});
+
+		burger.classList.toggle('toggle');
+	});
+
+  navLinks.forEach((link, index) => {
+    link.addEventListener('click', () => {
+
+      nav.classList.toggle('nav-active');
+
+      navLinks.forEach((link, index) => {
+        if (link.style.animation){
+          link.style.animation = '';
+        }
+      });
+
+      burger.classList.toggle('toggle');
+
+    });
+  });
 }
 
 navSlide();
