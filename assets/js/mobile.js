2957a64c7f07fee4a17e38bfdaab39e90d1e4be8
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-05-10 23:17:57 +0300

    Fix Linters errors

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,38 +1,35 @@
 const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-links-list');
-	const navLinks = document.querySelectorAll('.nav-links-list li');
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links-list');
+  const navLinks = document.querySelectorAll('.nav-links-list li');
 
-	burger.addEventListener('click', () => {
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
 
-		nav.classList.toggle('nav-active');
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    });
 
-		navLinks.forEach((link, index) => {
-			if (link.style.animation){
-				link.style.animation = '';
-			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-			}
-		});
+    burger.classList.toggle('toggle');
+  });
 
-		burger.classList.toggle('toggle');
-	});
-
-  navLinks.forEach((link, index) => {
+  navLinks.forEach((link) => {
     link.addEventListener('click', () => {
-
       nav.classList.toggle('nav-active');
 
-      navLinks.forEach((link, index) => {
-        if (link.style.animation){
+      navLinks.forEach((link) => {
+        if (link.style.animation) {
           link.style.animation = '';
         }
       });
 
       burger.classList.toggle('toggle');
-
     });
   });
-}
+};
 
 navSlide();
